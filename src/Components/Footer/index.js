fdb2d346031805d5e72af702202c23fbd4b260a4
Cbannerman <cbannerman21@gmail.com>
2022-08-05 13:03:24 -0400

    added footer

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+        <footer className="my-5">
+            <h2 id="footer">Social Meadia</h2>
+            <div className="my-2">
+                <li>
+                    <a href="https://www.instagram.com/c_a_bannerman/">
+                    <i className="nes-icon instagram is-small"></i> 
+                    </a>
+                    <a href ="https://github.com/Cbannerman21">
+                        <i className="nes-icon github is-small"></i> 
+                    </a>
+                    <a href="https://www.linkedin.com/in/chris-bannerman-79312081/">
+                        <i className="nes-icon linkedin is-small"></i>
+                    </a>
+                </li>
+            </div>
+                    </footer>
+    );
+}
+export default Footer;
